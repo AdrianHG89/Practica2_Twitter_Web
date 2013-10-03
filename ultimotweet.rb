3c6634b9ff52b00e40a7b6f692030a3e13f259f1
Adrian <adrihg89@gmail.com>
2013-10-03 22:45:00 +0100

    Arreglo del indexado

--- ultimotweet.rb
+++ ultimotweet.rb
@@ -8,14 +8,13 @@ require 'erb'
 
 class Twitts
 
-	#Inicializar variables
+	#Constructor
 	def initialize
 		@tweets = []
 		@nombre = ''
 		@num_tweets = 0		
 	end
 
-	#Acceso al HTML para mostrar los resultados
 	def erb(template)
   		template_file = File.open("ultimotweet.html.erb", 'r')
   		ERB.new(File.read(template_file)).result(binding)
@@ -23,24 +22,23 @@ class Twitts
 	
 	#Método call
 	def call env
-	    req = Rack::Request.new(env)
+		req = Rack::Request.new(env)
 	    
-	    binding.pry if ARGV[0]
-	   
-	   #Si no esta vacio , no es un espacio y el usuario existe en Twitter el nombre es el introducido
-	    @nombre = (req["txtnombre"] && req["txtnombre"] != '' && Twitter.user?(req["txtnombre"]) == true ) ? req["txtnombre"] : ''
+		binding.pry if ARGV[0]
+
+		@nombre = (req["txtnombre"] && req["txtnombre"] != '' && Twitter.user?(req["txtnombre"]) == true ) ? req["txtnombre"] : ''
 
 		@num_tweets = (req["txtnum"] && req["txtnum"].to_i>1 ) ? req["txtnum"].to_i : 1
 		puts "#{req["txtnum"]}"
-		
-		#Si el nombre existe buscamos sus últimos Tweets
+
+		#Comprobamos el nombre y si existe lanzamos la busqueda
 		if @nombre == req["txtnombre"]
 			puts "#{@tweets}"
 			ultimos_tweets = Twitter.user_timeline(@nombre,{:count=>@num_tweets.to_i})
 			@tweets =(@tweets && @tweets != '') ? ultimos_tweets.map{ |i| i.text} : ''				
 		end
 
-		#Invoca a erb
+		#Llamamos a erb
 		Rack::Response.new(erb('ultimotweet.html.erb'))
 	end
 
@@ -49,6 +47,6 @@ end
 
 Rack::Server.start(
 	:app => Twitts.new,
-    	:Port => 9494,
+    	:Port => 8080,
     	:server => 'thin'
 )
