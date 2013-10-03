3b20457c37029ce05450591b134c77991f01682c
Adrian <adrihg89@gmail.com>
2013-10-03 19:17:43 +0100

    cambiando

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-gem "twitter"
-gem "rack"
-gem "thin"
-gem "pry-debugger"
+gem 'faraday', '0.8.8'
+gem 'twitter'
+gem "rack", "~> 1.5.2" 
+gem "pry-debugger", "~> 0.2.2" #Degubber
+gem "thin", "~> 1.5.1" #servidor
--- ultimotweet.rb
+++ ultimotweet.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 require 'twitter'
 require './configure'
 require 'rack'
@@ -6,47 +6,53 @@ require 'pry-debugger'
 require 'thin'
 require 'erb'
 
-class UltimoTweet
-	
+class Twitts
 
-
-	def call env
-		#Constructor
-		def initialize
-			@tweets = []
-			@nombre = ''
-			@num_tweets = 0		
-		end
-
-		#Acceso al HTML para mostrar los resultados
-		def erb(template)
-	  		template_file = File.open("ultimotweet.html.erb", 'r')
-	  		ERB.new(File.read(template_file)).result(binding)
-		end
-
-		def call env
-			req = Rack::Request.new(env)
-	    		binding.pry if ARGV[0]
-	    		res['Content-Type'] = 'text/html'
-			@nombre = (req["txtusuario"] && req["txtusuario"] != '' && Twitter.user?(req["txtusuario"]) == true ) ? req["txtusuario"] : ''
-			@num_tweets = (req["txtnum"] && req["txtnum"].to_i>1 ) ? req["txtnum"].to_i : 1
-			puts "#{req["txtnum"]}"
-			#Si el nombre existe buscamos sus últimos Tweets
-			if @name == req["txtusuario"]
-				puts "#{@tweets}"
-				ultimos_tweet = Twitter.user_timeline(@nombre,{:count=>@num_tweets.to_i})
-				@tweets =(@tweets && @tweets != '') ? ultimos_tweet.map{ |i| i.text} : ''				
-			end
-		end
-
+	#Inicializar variables
+	def initialize
+		@todo_tweet = []
+		@name = ''
+		@number = 0		
 	end
+
+	#Acceso al HTML para mostrar los resultados
+	def erb(template)
+  		template_file = File.open("twitter.html.erb", 'r')
+  		ERB.new(File.read(template_file)).result(binding)
+	end
+	
+	#Método call
+	def call env
+	    req = Rack::Request.new(env)
+	    
+	    binding.pry if ARGV[0]
+	   
+	   #Si no esta vacio , no es un espacio y el usuario existe en Twitter el nombre es el introducido
+	    @name = (req["firstname"] && req["firstname"] != '' && Twitter.user?(req["firstname"]) == true ) ? req["firstname"] : ''
+
+		@number = (req["n"] && req["n"].to_i>1 ) ? req["n"].to_i : 1
+		puts "#{req["n"]}"
+		
+		#Si el nombre existe buscamos sus últimos Tweets
+		if @name == req["firstname"]
+			puts "#{@todo_tweet}"
+			ultimos_t = Twitter.user_timeline(@name,{:count=>@number.to_i})
+			@todo_tweet =(@todo_tweet && @todo_tweet != '') ? ultimos_t.map{ |i| i.text} : ''				
+		end
+
+		#Invoca a erb
+		Rack::Response.new(erb('twitter.html.erb'))
+	end
+
 end
-###############################################
-###############################################
-Rack::Server.start(
-  :app => UltimoTweet.new,
-  :Port => 9393,
-  :server => 'thin'
-)
-###############################################
-###############################################
+
+if $0 == __FILE__
+	Rack::Server.start(
+# 		:app => Rack::ShowExceptions.new(
+#        	Rack::Lint.new(
+#           	Rack::Twitts.new)), 
+		:app => Twitts.new,
+	    :Port => 9595,
+	    :server => 'thin'
+  	)
+end
